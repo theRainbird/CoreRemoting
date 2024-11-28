e564f594052e5545b8b6ab62910881fa78de71df
yallie <yallie@yandex.ru>
2024-11-28 22:59:49 +0300

    Implemented WwebsocketServerChannel and WebsocketConnection based on HttpListener web sockets.

---
+++ CoreRemoting/Channels/Websocket/WebsocketConnection.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using CoreRemoting.IO;
+
+namespace CoreRemoting.Channels.Websocket;
+
+/// <summary>
+/// Websocket connection.
+/// </summary>
+public class WebsocketConnection : IRawMessageTransport
+{
+    // note: LOH threshold is ~85 kilobytes
+    private const int BufferSize = 16 * 1024;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WebsocketConnection"/> class.
+    /// </summary>
+    public WebsocketConnection(HttpListenerWebSocketContext websocketContext, WebSocket websocket, IRemotingServer remotingServer)
+    {
+        WebSocketContext = websocketContext ?? throw new ArgumentNullException(nameof(websocketContext));
+        WebSocket = websocket ?? throw new ArgumentNullException(nameof(websocket));
+        RemotingServer = remotingServer ?? throw new ArgumentNullException(nameof(remotingServer));
+    }
+
+    public Guid Guid { get; } = Guid.NewGuid();
+
+    private HttpListenerWebSocketContext WebSocketContext { get; set; }
+
+    private WebSocket WebSocket { get; set; }
+
+    private IRemotingServer RemotingServer { get; set; }
+
+    private RemotingSession Session { get; set; }
+
+    /// <inheritdoc/>
+    public NetworkException LastException { get; set; }
+
+    /// <inheritdoc/>
+    public event Action<byte[]> ReceiveMessage;
+
+    /// <inheritdoc/>
+    public event Action<string, Exception> ErrorOccured;
+
+    /// <summary>
+    /// Event: fires when a web socket is disconnected.
+    /// </summary>
+    public event Action Disconnected;
+
+    /// <inheritdoc/>
+    public bool SendMessage(byte[] rawMessage)
+    {
+        try
+        {
+            var segment = new ArraySegment<byte>(rawMessage);
+            WebSocket.SendAsync(segment, WebSocketMessageType.Binary, true, CancellationToken.None)
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            LastException = ex as NetworkException ??
+                new NetworkException(ex.Message, ex);
+
+            ErrorOccured?.Invoke(ex.Message, LastException);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Starts listening to the incoming messages.
+    /// </summary>
+    public void StartListening()
+    {
+        CreateRemotingSession();
+        _ = ReadIncomingMessages();
+    }
+
+    /// <summary>
+    /// Creates <see cref="RemotingSession"/> for the incoming websocket connection.
+    /// </summary>
+    private void CreateRemotingSession()
+    {
+        byte[] clientPublicKey = null;
+
+        var messageEncryptionCookie = WebSocketContext.CookieCollection["MessageEncryption"];
+        if (messageEncryptionCookie?.Value == "1")
+        {
+            var shakeHandsCookie = WebSocketContext.CookieCollection["ShakeHands"];
+            clientPublicKey =
+                Convert.FromBase64String(
+                    shakeHandsCookie.Value);
+        }
+
+        Session = RemotingServer.SessionRepository.CreateSession(
+            clientPublicKey, RemotingServer, this);
+    }
+
+    private async Task ReadIncomingMessages()
+    {
+        var buffer = new byte[BufferSize];
+        var segment = new ArraySegment<byte>(buffer);
+
+        try
+        {
+            while (WebSocket.State == WebSocketState.Open)
+            {
+                var ms = new SmallBlockMemoryStream();
+                while (true)
+                {
+                    var result = await WebSocket.ReceiveAsync(segment,
+                        CancellationToken.None).ConfigureAwait(false);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                            string.Empty, CancellationToken.None).ConfigureAwait(false);
+
+                        Disconnected?.Invoke();
+                    }
+                    else
+                    {
+                        ms.Write(buffer, 0, result.Count);
+                    }
+
+                    if (result.EndOfMessage)
+                        break;
+                }
+
+                if (ms.Length > 0)
+                {
+                    // flush received websocket message
+                    var message = new byte[(int)ms.Length];
+                    ms.Position = 0;
+                    ms.Read(message, 0, message.Length);
+                    ReceiveMessage(message);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            LastException = ex as NetworkException ??
+                new NetworkException(ex.Message, ex);
+
+            ErrorOccured?.Invoke(ex.Message, LastException);
+            Disconnected?.Invoke();
+        }
+        finally
+        {
+            WebSocket?.Dispose();
+        }
+    }
+}
---
+++ CoreRemoting/Channels/Websocket/WebsocketServerChannel.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CoreRemoting.Channels.Websocket;
+
+/// <summary>
+/// Server side websocket channel implementation based on System.Net.Websockets and HttpListener.
+/// </summary>
+public class WebsocketServerChannel : IServerChannel
+{
+    private HttpListener HttpListener { get; set; }
+
+    private IRemotingServer Server { get; set; }
+
+    private ConcurrentDictionary<Guid, WebsocketConnection> Connections { get; } =
+        new ConcurrentDictionary<Guid, WebsocketConnection>();
+
+    /// <inheritdoc/>
+    public bool IsListening => HttpListener.IsListening;
+
+    /// <inheritdoc/>
+    public void Init(IRemotingServer server)
+    {
+        Server = server ?? throw new ArgumentNullException(nameof(server));
+        HttpListener = new HttpListener();
+
+        var prefix = "http://" +
+            Server.Config.HostName + ":" +
+            Server.Config.NetworkPort + "/";
+        HttpListener.Prefixes.Add(prefix);
+    }
+
+    /// <inheritdoc/>
+    public void StartListening()
+    {
+        _ = Task.Factory.StartNew(async () =>
+        {
+            HttpListener.Start();
+            while (IsListening)
+                await ReceiveConnection();
+        });
+    }
+
+    private async Task ReceiveConnection()
+    {
+        // check if it's a websocket request
+        var context = await HttpListener.GetContextAsync();
+        if (!context.Request.IsWebSocketRequest)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Response.Close();
+            return;
+        }
+
+        // accept websocket request and start a new session
+        var websocketContext = await context.AcceptWebSocketAsync(null);
+        var websocket = websocketContext.WebSocket;
+        var connection = new WebsocketConnection(websocketContext, websocket, Server);
+
+        // handle incoming websocket messages
+        Connections[connection.Guid] = connection;
+        connection.StartListening();
+    }
+
+    /// <inheritdoc/>
+    public void StopListening()
+    {
+        if (HttpListener != null && IsListening)
+            HttpListener.Stop();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        StopListening();
+        HttpListener = null;
+    }
+}
