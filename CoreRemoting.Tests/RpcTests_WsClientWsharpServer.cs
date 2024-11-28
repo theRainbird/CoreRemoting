46b076255a1e89d217dcfbf8ed6c981e4603b2f4
yallie <yallie@yandex.ru>
2024-11-28 23:16:54 +0300

    Added unit tests for all possible web socket channel combinations.

---
+++ CoreRemoting.Tests/RpcTests_WsClientWsharpServer.cs
@@ -0,0 +1,18 @@
+using CoreRemoting.Channels;
+using CoreRemoting.Channels.Websocket;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreRemoting.Tests
+{
+    public class RpcTests_WsClientWsharpServer : RpcTests
+    {
+        protected override IServerChannel ServerChannel => new WebsocketSharpServerChannel();
+
+        protected override IClientChannel ClientChannel => new WebsocketClientChannel();
+
+        public RpcTests_WsClientWsharpServer(ServerFixture fixture, ITestOutputHelper helper) : base(fixture, helper)
+        {
+        }
+    }
+}
