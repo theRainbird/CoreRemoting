8f58f5de38b6b60251f5e97ba9de81be358f2846
yallie <yallie@yandex.ru>
2024-11-30 06:24:32 +0300

    Added sketch channel based on QUIC network protocol (requires .NET 9.0).

---
+++ CoreRemoting.Channels.Quic/QuicServerConnection.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Net.Quic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreRemoting.Channels.Quic;
+
+/// <summary>
+/// Quic server-side connection.
+/// </summary>
+public class QuicServerConnection : IRawMessageTransport
+{
+    private const int MaxMessageSize = QuicClientChannel.MaxMessageSize;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuicServerConnection"/> class.
+    /// </summary>
+    public QuicServerConnection(QuicConnection connection, QuicStream stream, IRemotingServer remotingServer)
+    {
+        Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        ClientStream = stream ?? throw new ArgumentNullException(nameof(stream));
+        RemotingServer = remotingServer ?? throw new ArgumentNullException(nameof(remotingServer));
+        ClientReader = new BinaryReader(stream, Encoding.UTF8, true);
+        ClientWriter = new BinaryWriter(stream, Encoding.UTF8, true);
+    }
+
+    private QuicConnection Connection { get; set; }
+
+    private QuicStream ClientStream { get; set; }
+
+    private BinaryReader ClientReader { get; set; }
+
+    private BinaryWriter ClientWriter { get; set; }
+
+    private IRemotingServer RemotingServer { get; set; }
+
+    private RemotingSession Session { get; set; }
+
+    /// <inheritdoc/>
+    public NetworkException LastException { get; set; }
+
+    /// <inheritdoc/>
+    public event Action<byte[]> ReceiveMessage;
+
+    /// <inheritdoc/>
+    public event Action<string, Exception> ErrorOccured;
+
+    /// <summary>
+    /// Event: fires when a web socket is disconnected.
+    /// </summary>
+    public event Action Disconnected;
+
+    /// <inheritdoc/>
+    public bool SendMessage(byte[] rawMessage)
+    {
+        try
+        {
+            if (rawMessage.Length > MaxMessageSize)
+                throw new InvalidOperationException("Message is too large. Max size: " +
+                    MaxMessageSize + ", actual size: " + rawMessage.Length);
+
+            // message length + message body
+            ClientWriter.Write7BitEncodedInt(rawMessage.Length);
+            ClientWriter.Write(rawMessage, 0, rawMessage.Length);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            LastException = ex as NetworkException ??
+                new NetworkException(ex.Message, ex);
+
+            ErrorOccured?.Invoke(ex.Message, ex);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Starts listening to the incoming messages.
+    /// </summary>
+    public Guid StartListening()
+    {
+        var sessionId = CreateRemotingSession();
+        _ = Task.Factory.StartNew(() => ReadIncomingMessages());
+        return sessionId;
+    }
+
+    /// <summary>
+    /// Creates <see cref="RemotingSession"/> for the incoming QUIC connection.
+    /// </summary>
+    private Guid CreateRemotingSession()
+    {
+        // read handshake message
+        var clientPublicKey = ReadIncomingMessage();
+
+        // disable message encryption if handshake is empty
+        if (clientPublicKey != null && clientPublicKey.Length == 0)
+            clientPublicKey = null;
+
+        Session = RemotingServer.SessionRepository.CreateSession(
+            clientPublicKey, RemotingServer, this);
+
+        return Session.SessionId;
+    }
+
+    private byte[] ReadIncomingMessage()
+    {
+        var messageSize = ClientReader.Read7BitEncodedInt();
+        return ClientReader.ReadBytes(Math.Min(messageSize, MaxMessageSize));
+    }
+
+    private void ReadIncomingMessages()
+    {
+        try
+        {
+            while (true)
+            {
+                var message = ReadIncomingMessage();
+                if (message != null && message.Length > 0)
+                {
+                    // flush received QUIC message
+                    ReceiveMessage(message);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            LastException = ex as NetworkException ??
+                new NetworkException(ex.Message, ex);
+
+            ErrorOccured?.Invoke(ex.Message, LastException);
+            Disconnected?.Invoke();
+        }
+        finally
+        {
+            Connection?.DisposeAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            Connection = null;
+        }
+    }
+}
