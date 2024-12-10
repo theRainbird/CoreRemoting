07096726e5d69bd7b65741e38e7d57a1f594f1e0
yallie <yallie@yandex.ru>
2024-12-10 16:46:27 +0300

    Demonstrate the exception that can't be received by client, issue #105.

--- CoreRemoting/ClientRpcContext.cs
+++ CoreRemoting/ClientRpcContext.cs
@@ -17,17 +17,17 @@ namespace CoreRemoting
             UniqueCallKey = Guid.NewGuid();
             WaitHandle = new EventWaitHandle(initialState: false, EventResetMode.ManualReset);
         }
-        
+
         /// <summary>
         /// Gets the unique key of RPC call.
         /// </summary>
         public Guid UniqueCallKey { get; }
-        
+
         /// <summary>
         /// Gets or sets the result message, that was received from server after the call was invoked on server side.
         /// </summary>
         public MethodCallResultMessage ResultMessage { get; set; }
-        
+
         /// <summary>
         /// Gets or sets whether this RPC call is in error state.
         /// </summary>
@@ -37,12 +37,12 @@ namespace CoreRemoting
         /// Gets or sets an exception that describes an error that occurred on server side RPC invocation.
         /// </summary>
         public RemoteInvocationException RemoteException { get; set; }
-        
+
         /// <summary>
         /// Gets a wait handle that is set, when the response of this RPC call is received from server.
         /// </summary>
         public EventWaitHandle WaitHandle { get; } // TODO: replace with a Task?
-        
+
         /// <summary>
         /// Frees managed resources.
         /// </summary>
