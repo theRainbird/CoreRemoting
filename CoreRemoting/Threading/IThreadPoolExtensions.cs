1b2f598890db2a109e514104137acc1b653166f1
yallie <yallie@yandex.ru>
2025-03-07 17:00:01 +0300

    Added customizable thread pool for event handlers.

---
+++ CoreRemoting/Threading/IThreadPoolExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace CoreRemoting.Threading;
+
+/// <summary>
+/// Extension methods for the <see cref="IThreadPool"/> interface.
+/// </summary>
+public static class IThreadPoolExtensions
+{
+    /// <summary>
+    /// Queues a method for the execution, and specifies an object to be used by the method.
+    /// </summary>
+    /// <param name="threadPool">An instance of the <see cref="IThreadPool"/>.</param>
+    /// <param name="work">A <see cref="WaitCallback"/> representing the method to execute.</param>
+    public static void QueueUserWorkItem(this IThreadPool threadPool, WaitCallback work)
+    {
+        threadPool.QueueUserWorkItem(work, null);
+    }
+}
