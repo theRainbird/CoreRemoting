022ae24d3acd73cdc611e87d0e3d7b98d4cc2294
yallie <yallie@yandex.ru>
2025-03-12 18:04:44 +0300

    Implemented simple reader/writer lock based on Michel Raynal's pseudocode from «Concurrent Programming: Algorithms, Principles, and Foundations», pages 74-76, test on windows.

---
+++ CoreRemoting/Toolbox/AsyncReaderWriterLock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreRemoting.Toolbox;
+
+/// <summary>
+/// Simple async readers/writers lock.
+/// </summary>
+/// <remarks>
+/// Based on Michel Raynal's pseudocode using two mutexes and a counter:
+/// https://en.wikipedia.org/wiki/Readers–writer_lock#Using_two_mutexes
+/// </remarks>
+public class AsyncReaderWriterLock
+{
+    private volatile int blockingReaders = 0;
+
+    private AsyncLock ReadersLock { get; } = new();
+
+    private AsyncLock WritersLock { get; } = new();
+
+    private IDisposable ReleaseWritersLock { get; set; }
+
+    /// <summary>
+    /// Enter the lock in read mode.
+    /// </summary>
+    public async Task EnterReadLock()
+    {
+        using (await ReadersLock)
+        {
+            if (Interlocked.Increment(ref blockingReaders) == 1)
+            {
+                ReleaseWritersLock = await WritersLock;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reduces the recursion count in read mode and exits read mode if counter is zero.
+    /// </summary>
+    public async Task ExitReadLock()
+    {
+        using (await ReadersLock)
+        {
+            if (Interlocked.Decrement(ref blockingReaders) == 0)
+            {
+                ReleaseWritersLock?.Dispose();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Enter the lock in write mode.
+    /// </summary>
+    public async Task EnterWriteLock()
+    {
+        ReleaseWritersLock = await WritersLock;
+    }
+
+    /// <summary>
+    /// Releases the lock from write mode.
+    /// </summary>
+    public Task ExitWriteLock()
+    {
+        ReleaseWritersLock?.Dispose();
+        return Task.CompletedTask;
+    }
+}
