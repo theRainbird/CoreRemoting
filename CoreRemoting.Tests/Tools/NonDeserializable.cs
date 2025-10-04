b6aa73cca1c3351ac82f3a9149aded3758bf6bc4
yallie <yallie@yandex.ru>
2025-10-04 23:46:24 +0300

    Non-deserializable method parameter triggers invocation timeout, see #198.

---
+++ CoreRemoting.Tests/Tools/NonDeserializable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CoreRemoting.Tests.Tools
+{
+    [Serializable]
+    public class NonDeserializable
+    {
+        public NonDeserializable(int value) =>
+            Value = value;
+
+        public NonDeserializable() =>
+            Value = Throw();
+
+        public int Value { get; set; }
+
+        private int Throw() =>
+            throw new NotImplementedException();
+    }
+}
